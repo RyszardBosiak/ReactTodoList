d04444197d0019464daa0089e1537a975326e211
Ryszard Bosiak <ryszard.bosiak@lhsystems.com>
2019-12-04 23:25:51 +0100

    Fix misspelling in AppHeader component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.scss";
 import List from "./components/List/index";
-import AppHeader from "./components/AppHader/index";
+import AppHeader from "./components/AppHeader/index";
 
 function App() {
   return (
