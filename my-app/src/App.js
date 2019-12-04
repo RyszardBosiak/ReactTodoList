342d813542ad95b0a0dfbccc6e12cd103d227e50
Ryszard Bosiak <ryszard.bosiak@lhsystems.com>
2019-12-04 21:28:31 +0100

    Initial commit - create basic ToDo App on React

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.scss";
+import List from "./components/List/index";
+import AppHeader from "./components/AppHader/index";
+
+function App() {
+  return (
+    <div className="App">
+      <AppHeader></AppHeader>
+      <List></List>
+    </div>
+  );
+}
+
+export default App;
